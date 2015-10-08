7a418935f4d6ab5fd421480d9b846e04fe95dbac
MarcTowler <marc@marctowler.co.uk>
2015-10-09 00:58:55 +0100

    part two of blog-35
    updated the admin panel to include the new link. File is empty atm will
    be updated tomorrow

--- admin/menu.php
+++ admin/menu.php
@@ -3,9 +3,10 @@
     <li><a href="index.php">Admin Index</a></li>
     <li><a href="../" target="_blank">View Blog</a></li>
 	<li><a href='list-post.php'>Blog Posts</a></li>
-	<li><a href='categories.php'>Categories</a></li>
+	<li><a href='categories.php'>Categories</a>
+	<li><a href='file-manager.php'>Media Manager</a></li>
 	<li><a href='users.php'>Users</a></li>
-    <li><a href="comments.php">Moderate Comments</a></li>
+    <li><a href='comments.php'>Moderate Comments</a></li>
 	<li><a href='logout.php'>Logout</a></li>
 </ul>
 <div class='clear'></div>
