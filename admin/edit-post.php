ad9e55c9b6299c9708f8d304add72d1dc6ffc781
MarcTowler <marc@marctowler.co.uk>
2015-10-08 23:50:55 +0100

    again
    added config

--- admin/edit-post.php
+++ admin/edit-post.php
@@ -13,8 +13,27 @@ if(!$user->is_logged_in()){ header('Location: login.php'); }
   <link rel="stylesheet" href="../style/main.css">
   <script src="../js/ckeditor/ckeditor.js"></script>
 	<script>
-		CKEDITOR.replace( 'postDesc' );
-		CKEDITOR.replace( 'postCont' );
+		CKEDITOR.replaceAll();
+		CKEDITOR.editorConfig = function( config ) {
+			config.toolbarGroups = [
+				{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+				{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+				{ name: 'links', groups: [ 'links' ] },
+				{ name: 'insert', groups: [ 'insert' ] },
+				{ name: 'forms', groups: [ 'forms' ] },
+				{ name: 'tools', groups: [ 'tools' ] },
+				{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
+				{ name: 'others', groups: [ 'others' ] },
+				'/',
+				{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+				{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
+				{ name: 'styles', groups: [ 'styles' ] },
+				{ name: 'colors', groups: [ 'colors' ] },
+				{ name: 'about', groups: [ 'about' ] }
+			];
+
+			config.removeButtons = 'Underline,Subscript,Superscript,Cut,Copy,Paste,PasteText,PasteFromWord';
+		};
   </script>
 </head>
 <body>
