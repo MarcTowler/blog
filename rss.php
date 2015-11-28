6f1baea1253908150ff8cb501e08f8c6fb936f52
MarcTowler <marc@marctowler.co.uk>
2015-11-28 13:57:33 +0000

    Fix for issue blog-52, RSS Feed now has the following: - GUID links - pubDate is now RFC 822 valid date-time - Links to posts now are SEO based rather then viewpost.php?id=# - <![CDATA[]]> now added to description - Feed is better formatted for visual inspection - charset updated to UTF-8 - atom self ref link is now added

--- rss.php
+++ rss.php
@@ -1,34 +1,35 @@
 <?php
 
-header("Content-Type: application/rss+xml; charset=ISO-8859-1");
+header("Content-Type: application/rss+xml; charset=UTF-8");
 
 require_once('includes/config.php');
 
-$rssfeed = '<?xml version="1.0" encoding="ISO-8859-1"?>';
-$rssfeed .= '<rss version="2.0">';
-$rssfeed .= '<channel>';
-$rssfeed .= '<title>' . SITENAME . '</title>';
-$rssfeed .= '<link>https://www.marctowler.co.uk/</link>';
-$rssfeed .= '<description>Website Development and Design blog written by Marc Towler, includes game
-                    reviews and book reviews</description>';
-$rssfeed .= '<language>en-gb</language>';
-$rssfeed .= '<copyright>Copyright (C) 2012-2015 marctowler.co.uk</copyright>';
+$rssfeed = "<?xml version='1.0' encoding='UTF-8'?>\n";
+$rssfeed .= "<rss version='2.0' xmlns:atom='http://www.w3.org/2005/Atom'>\n";
+$rssfeed .= "<channel>\n";
+$rssfeed .= "<title>" . SITENAME . "</title>\n";
+$rssfeed .= "<atom:link href='https://www.marctowler.co.uk/rss.php' rel='self' type='application/rss+xml' />\n";
+$rssfeed .= "<link>https://www.marctowler.co.uk/</link>\n";
+$rssfeed .= "<description>Website Development and Design blog written by Marc Towler, includes game reviews and book reviews</description>\n";
+$rssfeed .= "<language>en-gb</language>\n";
+$rssfeed .= "<copyright>Copyright (C) 2012-2015 marctowler.co.uk</copyright>\n";
 
 $stmt = $db->query("SELECT * FROM blog_posts_seo WHERE published=1 ORDER BY postID DESC");
 
 
 while($row = $stmt->fetch())
 {
-    $rssfeed .= '<item>';
-    $rssfeed .= '<title>' . $row["postTitle"] . '</title>';
-    $rssfeed .= '<description>' .  html_entity_decode(strip_tags($row["postDesc"])) . '</description>';
-    $rssfeed .= '<link>https://marctowler.co.uk/viewpost.php?id=' . $row["postID"] . '</link>';
-    $rssfeed .= '<pubDate>' .date("jS M Y H:i:s", strtotime($row["postDate"])) . '</pubDate>';
-    $rssfeed .= '</item>';
+    $rssfeed .= "<item>\n";
+    $rssfeed .= "<title>" . $row["postTitle"] . "</title>\n";
+    $rssfeed .= "<description><![CDATA[" .  html_entity_decode(strip_tags($row['postDesc'])) . "]]></description>\n";
+    $rssfeed .= "<link>https://www.marctowler.co.uk/" . $row['postSlug'] . ".html</link>\n";
+    $rssfeed .= "<guid>https://www.marctowler.co.uk/" . $row['postSlug'] . ".html</guid>\n";
+    $rssfeed .= "<pubDate>" .date('D, d M Y H:i:s', strtotime($row['postDate'])) . " GMT</pubDate>\n";
+    $rssfeed .= "</item>\n";
 }
 
-$rssfeed .= '</channel>';
-$rssfeed .= '</rss>';
+$rssfeed .= "</channel>\n";
+$rssfeed .= "</rss>\n";
 
 echo $rssfeed;
 ?>
