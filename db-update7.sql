833c474d61883d1181b380c1baffa5acc4d34d4b
MarcTowler <marc@marctowler.co.uk>
2015-12-05 21:49:50 +0000

    Fix for blog-58
    Added to db-update script to fix the 127 cap on views

--- db-update7.sql
+++ db-update7.sql
@@ -1,1 +1,5 @@
+ALTER TABLE `blog_posts_seo`
 ADD COLUMN `author` TINYINT(1) NOT NULL DEFAULT 0 AFTER `published`;
+
+ALTER TABLE `blog_posts_seo`
+CHANGE COLUMN `views` `views` INT(10) NOT NULL DEFAULT '0' COMMENT '' ;
