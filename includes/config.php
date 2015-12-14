40676eb6448eee7f8a2458d300fdbe8ab7cbb614
MarcTowler <marc@marctowler.co.uk>
2015-12-14 19:53:46 +0000

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -4,9 +4,9 @@ session_start();
 
 //database credentials
 define('DBHOST','localhost');
-define('DBUSER','root');
-define('DBPASS','06061990');
-define('DBNAME','c1blog');
+define('DBUSER','');
+define('DBPASS','');
+define('DBNAME',''');
 define('PORT'  ,'3306');
 
 define('SITENAME',    'Sample Name');
