73eef52f6ecf9a76e416188b955bff1adc5b6b72
MarcTowler <marc@marctowler.co.uk>
2016-01-05 23:19:10 +0000

    added Heap Analytics

--- archives.php
+++ archives.php
@@ -21,7 +21,10 @@ require_once('includes/config.php');
     <link rel="stylesheet" href="css/normalize.css" />
     <link rel="stylesheet" href="css/main.css">
     <link rel="alternate" href="rss.php" title="My RSS feed" type="application/rss+xml" />
-    <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+    <script type="text/javascript">
+        window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var n=t.forceSSL||"https:"===document.location.protocol,a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src=(n?"https:":"http:")+"//cdn.heapanalytics.com/js/heap-"+e+".js";var o=document.getElementsByTagName("script")[0];o.parentNode.insertBefore(a,o);for(var r=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["clearEventProperties","identify","setEventProperties","track","unsetEventProperty"],c=0;c<p.length;c++)heap[p[c]]=r(p[c])};
+        heap.load("14710187");
+    </script>
 </head>
 <body>
 <!--[if lt IE 7]>
